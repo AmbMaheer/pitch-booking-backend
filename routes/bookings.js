fb4f6e00499c0a1678ba39271c42c226e6635e8f
AmbMaheer <ambmaheer@gmail.com>
2025-06-29 22:09:06 +0100

    Ensure Pitch models file is tracked

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Booking = require('../models/booking');
-const Pitch = require('../models/pitch');
+const Pitch = require('../models/Pitch');
 const router = express.Router();
 
 // GET all bookings
